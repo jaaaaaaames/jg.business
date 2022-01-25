ad0709451c10200a245bd1b8abeab3c846b163db
James <63654278+6jg@users.noreply.github.com>
2022-01-25 16:51:41 +0000

    Favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-black text-white">
       <Head>
-        <title>Create Next App</title>
+        <title>jg</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
